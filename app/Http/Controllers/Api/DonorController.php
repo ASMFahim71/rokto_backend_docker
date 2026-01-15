7e12a21b38d74248b908ceaae0a6b9be7a9e6323
Ahsanul24 <ahsanulimam10923@gmail.com>
2026-01-15 23:28:52 +0600

    fix issues

--- app/Http/Controllers/Api/DonorController.php
+++ app/Http/Controllers/Api/DonorController.php
@@ -29,11 +29,6 @@ class DonorController extends Controller
             ['user_id' => $user->id],
             [
                 'last_donation_date' => $request->last_donation_date,
-                // If creating for the first time, we might want to set blood_group_id from user if available, 
-                // but since the user table has it now, maybe we should sync it or just rely on user table.
-                // However, the donors table has a blood_group_id column as per schema.
-                // Let's assume we should sync it if user has it, or require it. 
-                // For now, I'll update it if the user has it set.
                 'blood_group_id' => $user->blood_group_id,
             ]
         );
@@ -82,6 +77,9 @@ class DonorController extends Controller
             ->join('users', 'donors.user_id', '=', 'users.id')
             ->where('donors.is_available', 1)
             ->whereDate('donors.last_donation_date', '<=', now()->subDays(90))
+            ->when($request->blood_group_id, function ($query) use ($request) {
+                return $query->where('donors.blood_group_id', $request->blood_group_id);
+            })
             ->with([
                 'user:id,name,phone,division_id,district_id,upazila_id',
                 'user.division:id,name',
